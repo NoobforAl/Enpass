f9e9eaa5c58995411d6d10957479a9865a735ad3
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-07-05 18:47:06 +0330

    fix value pkg and match with crypto pkg
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

--- src/database/values.go
+++ src/database/values.go
@@ -1,74 +1,54 @@
 package database
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
 	"crypto/sha256"
-	"encoding/base64"
 	"encoding/hex"
-	"errors"
-	"io"
+
+	"github.com/NoobforAl/Enpass/crypto"
+	errs "github.com/NoobforAl/Enpass/errors"
 )
 
-type Value string
-
 type Values struct {
-	UserName Value
-	Password Value
-	Note     Value
-	Hash     Value
+	UserName string
+	Password string
+	Note     string
+	Hash     string
 }
 
-func (v Value) EncryptValue(key string) (Value, error) {
-	tmp, err := encrypt(key, v.String())
-	return Value(tmp), err
-}
-
-func (v Value) DecryptValue(key string) (Value, error) {
-	tmp, err := decrypt(key, v.String())
-	return Value(tmp), err
-}
-
-func (v Value) HashSha256() Value {
+func HashSha256(v string) string {
 	h := sha256.Sum224([]byte(v))
-	return Value(hex.EncodeToString(h[:]))
+	return hex.EncodeToString(h[:])
 }
 
-func (v Value) IsOkHash(s string) bool {
-	val := Value(s)
-	return val.HashSha256() == v
-}
-
-func (v Value) String() string {
-	return string(v)
+func IsOkHash(s string, hash string) bool {
+	return HashSha256(s) == hash
 }
 
 func (v *Values) EncryptValues(key string) error {
 	var (
-		UserName Value
-		Password Value
-		Note     Value
+		UserName string
+		Password string
+		Note     string
 		err      error
 	)
 
-	UserName, err = v.UserName.EncryptValue(key)
+	UserName, err = crypto.Encrypt(key, v.UserName)
 	if err != nil {
 		return err
 	}
 
-	Password, err = v.Password.EncryptValue(key)
+	Password, err = crypto.Encrypt(key, v.Password)
 	if err != nil {
 		return err
 	}
 
-	Note, err = v.Note.EncryptValue(key)
+	Note, err = crypto.Encrypt(key, v.Note)
 	if err != nil {
 		return err
 	}
 
 	v.Hash = v.UserName + v.Password + v.Note
-	v.Hash = v.Hash.HashSha256()
+	v.Hash = HashSha256(v.Hash)
 
 	v.UserName = UserName
 	v.Password = Password
@@ -78,23 +58,23 @@ func (v *Values) EncryptValues(key string) error {
 
 func (v *Values) DecryptValues(key string) error {
 	var (
-		UserName Value
-		Password Value
-		Note     Value
+		UserName string
+		Password string
+		Note     string
 		err      error
 	)
 
-	UserName, err = v.UserName.DecryptValue(key)
+	UserName, err = crypto.Decrypt(key, v.UserName)
 	if err != nil {
 		return err
 	}
 
-	Password, err = v.Password.DecryptValue(key)
+	Password, err = crypto.Decrypt(key, v.Password)
 	if err != nil {
 		return err
 	}
 
-	Note, err = v.Note.DecryptValue(key)
+	Note, err = crypto.Decrypt(key, v.Note)
 	if err != nil {
 		return err
 	}
@@ -103,64 +83,9 @@ func (v *Values) DecryptValues(key string) error {
 	v.Password = Password
 	v.Note = Note
 
-	if !v.Hash.IsOkHash(string(UserName + Password + Note)) {
-		return ErrHashNotMatch
+	if IsOkHash(UserName+Password+Note, v.Hash) {
+		return errs.ErrHashNotMatch
 	}
 
 	return nil
-}
-
-func encrypt(key, text string) (string, error) {
-	block, err := aes.NewCipher([]byte(key))
-	if err != nil {
-		return "", errors.Join(ErrEncrypt, err)
-	}
-
-	nonce := make([]byte, 12)
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		return "", errors.Join(ErrEncrypt, err)
-	}
-
-	aesgcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return "", errors.Join(ErrEncrypt, err)
-	}
-
-	ciphertext := aesgcm.Seal(nil, nonce, []byte(text), nil)
-	ciphertext = append(nonce, ciphertext...)
-
-	ciphertextString := base64.StdEncoding.EncodeToString(ciphertext)
-	return ciphertextString, nil
-}
-
-func decrypt(key, text string) (string, error) {
-	textDecoded, err := base64.StdEncoding.DecodeString(text)
-	if err != nil {
-		return "", errors.Join(ErrDecrypt, err)
-	}
-
-	nonceSize := 12
-	if len(textDecoded) < nonceSize {
-		return "", errors.Join(ErrDecrypt, ErrTextIsShort)
-	}
-	nonce := textDecoded[:nonceSize]
-	textDecoded = textDecoded[nonceSize:]
-
-	block, err := aes.NewCipher([]byte(key))
-	if err != nil {
-		return "", errors.Join(ErrDecrypt, err)
-	}
-
-	aesgcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return "", errors.Join(ErrDecrypt, err)
-	}
-
-	plaintextBytes, err := aesgcm.Open(nil, nonce, textDecoded, nil)
-	if err != nil {
-		return "", errors.Join(ErrDecrypt, err)
-	}
-
-	plaintext := string(plaintextBytes)
-	return plaintext, nil
 }
