9d42a2b7132acd0ecc98851bccb333118ba79d18
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-07-11 14:13:17 +0330

    fix formate function and methods
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

--- src/router/router.go
+++ src/router/router.go
@@ -6,27 +6,46 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Default(r *gin.Engine, s contract.Stor) {
-	r.GET("/genRandomPass", controller.GenRandomPass)
-	r.POST("/login", controller.Login(s))
+func Default(
+	r *gin.Engine,
+	s contract.Store,
+) {
+	r.POST("/login",
+		controller.Login(s))
+	r.GET("/genRandomPass",
+		controller.GenRandomPass)
 }
 
-func MainApi(api *gin.RouterGroup, s contract.Stor) {
+func MainApi(
+	api *gin.RouterGroup,
+	s contract.Store,
+) {
 	api.Use(controller.AuthMiddleware())
 
-	api.GET("/allPass", controller.AllPass(s))
-	api.GET("/allService", controller.AllService(s))
+	api.GET("/allPass",
+		controller.AllPass(s))
+	api.GET("/allService",
+		controller.AllService(s))
 
-	api.GET("/pass/:id", controller.FindPass(s))
-	api.GET("/service/:id", controller.FindService(s))
+	api.GET("/pass/:id",
+		controller.FindPass(s))
+	api.GET("/service/:id",
+		controller.FindService(s))
 
-	api.POST("/createPass", controller.NewPass(s))
-	api.POST("/createService", controller.NewService(s))
+	api.POST("/createPass",
+		controller.NewPass(s))
+	api.POST("/createService",
+		controller.NewService(s))
 
-	api.PUT("/updateUser", controller.UpdateUser(s))
-	api.PUT("/updatePass", controller.UpdatePass(s))
-	api.PUT("/updateService", controller.UpdateService(s))
+	api.PUT("/updateUser",
+		controller.UpdateUser(s))
+	api.PUT("/updatePass",
+		controller.UpdatePass(s))
+	api.PUT("/updateService",
+		controller.UpdateService(s))
 
-	api.DELETE("/deletePass/:id", controller.DeletePassWord(s))
-	api.DELETE("/deleteService/:id", controller.DeleteService(s))
+	api.DELETE("/deletePass/:id",
+		controller.DeletePassWord(s))
+	api.DELETE("/deleteService/:id",
+		controller.DeleteService(s))
 }
