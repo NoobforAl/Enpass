862384599da3a280d9250dfc264cc12c1f37a3b0
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-07-12 13:04:02 +0330

    fix fomate func & add const nonce size
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

--- src/crypto/decrypt.go
+++ src/crypto/decrypt.go
@@ -9,32 +9,45 @@ import (
 	errs "github.com/NoobforAl/Enpass/errors"
 )
 
-func Decrypt(key, text string) (string, error) {
-	textDecoded, err := base64.StdEncoding.DecodeString(text)
+func Decrypt(
+	key, text string,
+) (string, error) {
+	textDecoded, err := base64.
+		StdEncoding.
+		DecodeString(text)
+
 	if err != nil {
-		return "", errors.Join(errs.ErrDecrypt, err)
+		return "",
+			errors.Join(errs.ErrDecrypt, err)
 	}
 
-	nonceSize := 12
 	if len(textDecoded) < nonceSize {
-		return "", errors.Join(errs.ErrDecrypt, errs.ErrTextIsShort)
+		return "",
+			errors.Join(errs.ErrDecrypt,
+				errs.ErrTextIsShort)
 	}
+
 	nonce := textDecoded[:nonceSize]
 	textDecoded = textDecoded[nonceSize:]
 
 	block, err := aes.NewCipher([]byte(key))
 	if err != nil {
-		return "", errors.Join(errs.ErrDecrypt, err)
+		return "",
+			errors.Join(errs.ErrDecrypt, err)
 	}
 
 	aesgcm, err := cipher.NewGCM(block)
 	if err != nil {
-		return "", errors.Join(errs.ErrDecrypt, err)
+		return "",
+			errors.Join(errs.ErrDecrypt, err)
 	}
 
-	plaintextBytes, err := aesgcm.Open(nil, nonce, textDecoded, nil)
+	plaintextBytes, err := aesgcm.Open(
+		nil, nonce, textDecoded, nil)
+
 	if err != nil {
-		return "", errors.Join(errs.ErrDecrypt, err)
+		return "",
+			errors.Join(errs.ErrDecrypt, err)
 	}
 
 	plaintext := string(plaintextBytes)
--- src/crypto/encrypt.go
+++ src/crypto/encrypt.go
@@ -11,13 +11,15 @@ import (
 	errs "github.com/NoobforAl/Enpass/errors"
 )
 
+const nonceSize = 12
+
 func Encrypt(key, text string) (string, error) {
 	block, err := aes.NewCipher([]byte(key))
 	if err != nil {
 		return "", errors.Join(errs.ErrEncrypt, err)
 	}
 
-	nonce := make([]byte, 12)
+	nonce := make([]byte, nonceSize)
 	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
 		return "", errors.Join(errs.ErrEncrypt, err)
 	}
