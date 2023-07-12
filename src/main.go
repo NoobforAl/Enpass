2fa046c7c6e01bf929cf40f1acf754227ede3b4b
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-07-12 21:14:19 +0330

    fix main file with new change
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

--- src/main.go
+++ src/main.go
@@ -3,21 +3,23 @@ package main
 import (
 	env "github.com/NoobforAl/Enpass/config_loader"
 	"github.com/NoobforAl/Enpass/database"
-	"github.com/NoobforAl/Enpass/router"
+	"github.com/NoobforAl/Enpass/http/v1/router"
+	"github.com/NoobforAl/Enpass/validation"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	stor, err := database.New(env.GetDSN())
+	validator := validation.New()
+	store, err := database.New(env.GetDSN())
 	if err != nil {
 		panic(err)
 	}
 
 	r := gin.Default()
-	router.Default(r, stor)
+	router.Default(r, store, validator)
 
 	api := r.Group("/api")
-	router.MainApi(api, stor)
+	router.MainApi(api, store, validator)
 
 	if err := r.Run("127.0.0.1:1111"); err != nil {
 		panic(err)
