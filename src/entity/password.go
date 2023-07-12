b0cb6938b2001321f178592272563cf029d29122
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-07-12 12:41:14 +0330

    remove validate tag
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

---
+++ src/entity/password.go
@@ -0,0 +1,10 @@
+package entity
+
+type Password struct {
+	PassID    uint   `json:"passid"`
+	UserID    uint   `json:"userid"`
+	ServiceID uint   `json:"serviceid"`
+	UserName  string `json:"username"`
+	Password  string `json:"password"`
+	Note      string `json:"note"`
+}
