1eec63cf557470d0f3a5e88813bb940f347a69be
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-07-12 13:05:36 +0330

    fix formate encrypte func
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

--- src/crypto/encrypt.go
+++ src/crypto/encrypt.go
@@ -13,25 +13,31 @@ import (
 
 const nonceSize = 12
 
-func Encrypt(key, text string) (string, error) {
+func Encrypt(
+	key, text string,
+) (string, error) {
 	block, err := aes.NewCipher([]byte(key))
 	if err != nil {
-		return "", errors.Join(errs.ErrEncrypt, err)
+		return "",
+			errors.Join(errs.ErrEncrypt, err)
 	}
 
 	nonce := make([]byte, nonceSize)
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+	if _, err := io.ReadFull(
+		rand.Reader, nonce,
+	); err != nil {
 		return "", errors.Join(errs.ErrEncrypt, err)
 	}
 
 	aesgcm, err := cipher.NewGCM(block)
 	if err != nil {
-		return "", errors.Join(errs.ErrEncrypt, err)
+		return "",
+			errors.Join(errs.ErrEncrypt, err)
 	}
 
 	ciphertext := aesgcm.Seal(nil, nonce, []byte(text), nil)
 	ciphertext = append(nonce, ciphertext...)
 
-	ciphertextString := base64.StdEncoding.EncodeToString(ciphertext)
-	return ciphertextString, nil
+	return base64.StdEncoding.
+		EncodeToString(ciphertext), nil
 }
