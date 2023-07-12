3495583f1b882ff8641365715675873bba6681ea
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-07-12 23:23:04 +0330

    add auth file for auth jwt token
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

---
+++ src/http/v1/controller/auth.go
@@ -0,0 +1,67 @@
+package controller
+
+import (
+	crand "crypto/rand"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	env "github.com/NoobforAl/Enpass/config_loader"
+	errs "github.com/NoobforAl/Enpass/errors"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+var secretKey = make([]byte, 32)
+
+const userId = "userId"
+
+func init() {
+	_, err := crand.Read(secretKey)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+
+		if tokenString == "" {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, errors.Join(errs.ErrUnexpectedSigning, fmt.Errorf("%v", token.Header["alg"]))
+			}
+			return secretKey, nil
+		})
+
+		if err != nil {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok || !token.Valid {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		id := fmt.Sprintf("%v", claims["id"])
+		c.Set(userId, id)
+		c.Next()
+	}
+}
+
+func generateToken(id uint) (string, error) {
+	claims := jwt.MapClaims{
+		"id":  id,
+		"exp": time.Now().Add(env.GetLifeTime()).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(secretKey)
+}
