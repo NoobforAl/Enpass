3c5dbc61b3a1bcfa6fbce50d2b45c637ee93501d
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-07-13 13:44:49 +0330

    fix interactor user for save and check password in cache
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

--- src/interactor/user.go
+++ src/interactor/user.go
@@ -3,22 +3,57 @@ package interactor
 import (
 	"context"
 
+	"github.com/NoobforAl/Enpass/caching"
+	"github.com/NoobforAl/Enpass/crypto"
 	"github.com/NoobforAl/Enpass/entity"
+	errs "github.com/NoobforAl/Enpass/errors"
 )
 
 func (i interActor) FindUser(
 	ctx context.Context,
 	user entity.User,
 ) (entity.User, error) {
-	return i.store.GetUser(ctx, user)
+	u, err := i.store.GetUser(ctx, user)
+	if err != nil {
+		return user, err
+	}
+
+	p, err := crypto.Decrypt(
+		user.Password, u.Password)
+
+	if err != nil {
+		return user, err
+	}
+
+	if !crypto.IsOkHash(user.Password, p) {
+		return user, errs.ErrNotMatchPassword
+	}
+
+	if _, err = caching.CachedPass.
+		GetPass(user.ID); err != nil {
+		go caching.CachedPass.DeletePass(user.ID)
+	}
+
+	caching.CachedPass.SetPass(user.ID, user.Password)
+	return user, nil
 }
 
 func (i interActor) UpdateUser(
 	ctx context.Context,
-	oldUser entity.User,
-	newUser entity.User,
+	old entity.User,
+	new entity.User,
 ) (entity.User, error) {
+	password, err := caching.CachedPass.
+		GetPass(old.ID)
+
+	if err != nil {
+		return new, err
+	}
+
+	if password != old.Password {
+		return new, errs.ErrNotMatchPassword
+	}
+
 	return i.store.UpdateUser(
-		ctx, oldUser, newUser,
-	)
+		ctx, old, new)
 }
