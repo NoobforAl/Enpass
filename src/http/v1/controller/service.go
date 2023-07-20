a03829370d11f0fa77726d136fdba29de3aec278
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-07-20 12:19:57 +0330

    fix endpoint service
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

--- src/http/v1/controller/service.go
+++ src/http/v1/controller/service.go
@@ -3,36 +3,49 @@ package controller
 import (
 	"net/http"
 
-	errs "github.com/NoobforAl/Enpass/errors"
-
 	"github.com/NoobforAl/Enpass/contract"
-	"github.com/NoobforAl/Enpass/entity"
+	errs "github.com/NoobforAl/Enpass/errors"
+	"github.com/NoobforAl/Enpass/http/v1/parser"
+	"github.com/NoobforAl/Enpass/interactor"
+	"github.com/NoobforAl/Enpass/schema"
 	"github.com/gin-gonic/gin"
 )
 
-func NewService(stor contract.Store) gin.HandlerFunc {
+func NewService(
+	stor contract.Store,
+	validator contract.Validation,
+) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var ser entity.Service
+		var ser schema.Service
 		var err error
 
-		if err = ser.Pars(c); err != nil {
+		if err = validator.
+			ParsService(c, &ser); err != nil {
 			errs.ErrHandle(c, err)
 			return
 		}
 
-		if err = ser.CreateService(c, stor); err != nil {
+		service := parser.SchemaToEntityService(ser, 0)
+		service, err = interactor.New(stor).CreateService(c, service)
+		if err != nil {
 			errs.ErrHandle(c, err)
 			return
 		}
 
-		c.JSON(http.StatusOK, ser)
+		c.JSON(http.StatusOK, service)
 	}
 }
 
-func AllService(stor contract.Store) gin.HandlerFunc {
+func AllService(
+	stor contract.Store,
+	validator contract.Validation,
+) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var ser entity.Service
-		services, err := ser.GetAllService(c, stor)
+
+		services, err := interactor.
+			New(stor).
+			GetAllService(c)
+
 		if err != nil {
 			errs.ErrHandle(c, err)
 			return
@@ -42,58 +55,89 @@ func AllService(stor contract.Store) gin.HandlerFunc {
 	}
 }
 
-func FindService(stor contract.Store) gin.HandlerFunc {
+func FindService(
+	stor contract.Store,
+	validator contract.Validation,
+) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		serviceId, err := getParmInt(c, "id")
+		id, err := parser.GetParmInt(c, "id")
 		if err != nil {
 			errs.ErrHandle(c, err)
 			return
 		}
 
-		ser := &entity.Service{ServiceId: uint(serviceId)}
-		ser, err = ser.FindService(c, stor)
+		service := parser.SchemaToEntityService(
+			schema.Service{}, uint(id),
+		)
+
+		service, err = interactor.New(stor).
+			FindService(c, service)
 		if err != nil {
 			errs.ErrHandle(c, err)
 			return
 		}
 
-		c.JSON(http.StatusOK, ser)
+		c.JSON(http.StatusOK, service)
 	}
 }
 
-func UpdateService(stor contract.Store) gin.HandlerFunc {
+func UpdateService(
+	stor contract.Store,
+	validator contract.Validation,
+) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var ser entity.Service
+		var ser schema.Service
 		var err error
 
-		if err = ser.Pars(c); err != nil {
+		if err = validator.
+			ParsService(c, &ser); err != nil {
 			errs.ErrHandle(c, err)
 			return
 		}
 
-		if err = ser.UpdateService(c, stor); err != nil {
-			errs.ErrHandle(c, err)
-			return
-		}
-
-		c.JSON(http.StatusOK, ser)
-	}
-}
-
-func DeleteService(stor contract.Store) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		serviceId, err := getParmInt(c, "id")
+		id, err := parser.GetParmInt(c, "id")
 		if err != nil {
 			errs.ErrHandle(c, err)
 			return
 		}
 
-		ser := entity.Service{ServiceId: uint(serviceId)}
-		if err = ser.DeleteService(c, stor); err != nil {
+		service := parser.SchemaToEntityService(ser, uint(id))
+
+		service, err = interactor.
+			New(stor).
+			UpdateService(c, service)
+
+		if err != nil {
 			errs.ErrHandle(c, err)
 			return
 		}
 
-		c.JSON(http.StatusOK, ser)
+		c.JSON(http.StatusOK, service)
+	}
+}
+
+func DeleteService(
+	stor contract.Store,
+	validator contract.Validation,
+) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id, err := parser.GetParmInt(c, "id")
+		if err != nil {
+			errs.ErrHandle(c, err)
+			return
+		}
+
+		service := parser.SchemaToEntityService(
+			schema.Service{}, uint(id),
+		)
+
+		service, err = interactor.New(stor).
+			DeleteService(c, service)
+		if err != nil {
+			errs.ErrHandle(c, err)
+			return
+		}
+
+		c.JSON(http.StatusOK, service)
 	}
 }
