fad5984367780b6345bf9fa6fadf9a660e7d74c2
NoobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-08-06 23:22:40 +0330

    match interactor with logger
    Signed-off-by: NoobforAl <58856931+NoobforAl@users.noreply.github.com>

--- src/interactor/user.go
+++ src/interactor/user.go
@@ -13,11 +13,13 @@ func (i interActor) FindUser(
 	ctx context.Context,
 	user entity.User,
 ) (entity.User, error) {
+	i.log.Debug("Find User")
 	u, err := i.store.GetUser(ctx, user)
 	if err != nil {
 		return user, err
 	}
 
+	i.log.Debug("check decrypt password")
 	p, err := crypto.Decrypt(
 		user.Password, u.Password)
 
@@ -25,35 +27,41 @@ func (i interActor) FindUser(
 		return user, err
 	}
 
+	i.log.Debug("check hash password")
 	if !crypto.IsOkHash(user.Password, p) {
 		return user, errs.ErrNotMatchPassword
 	}
 
 	if _, err = caching.CachedPass.
 		GetPass(user.ID); err != nil {
+		i.log.Debug("cache pass delete not found," +
+			"start new delete password cache," +
+			"after set timed.")
 		go caching.CachedPass.DeletePass(user.ID)
 	}
 
+	i.log.Debug("set password in cache")
 	caching.CachedPass.SetPass(user.ID, user.Password)
 	return user, nil
 }
 
 func (i interActor) UpdateUser(
 	ctx context.Context,
-	old entity.User,
-	new entity.User,
+	old, new entity.User,
 ) (entity.User, error) {
-	password, err := caching.CachedPass.
-		GetPass(old.ID)
+	var pass string
+	var err error
 
-	if err != nil {
+	i.log.Debug("update user password")
+	if pass, err = caching.
+		CachedPass.
+		GetPass(old.ID); err != nil {
 		return new, err
 	}
 
-	if password != old.Password {
+	if pass != old.Password {
 		return new, errs.ErrNotMatchPassword
 	}
 
-	return i.store.UpdateUser(
-		ctx, old, new)
+	return i.store.UpdateUser(ctx, old, new)
 }
