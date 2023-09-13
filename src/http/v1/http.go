6cf52274d56ea66ce7a84389e3dfa31689396d62
NoobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-09-13 19:28:09 +0330

    add httpApp for esay setup app
    Signed-off-by: NoobforAl <58856931+NoobforAl@users.noreply.github.com>

---
+++ src/http/v1/http.go
@@ -0,0 +1,35 @@
+package http
+
+import (
+	env "github.com/NoobforAl/Enpass/config_loader"
+	"github.com/NoobforAl/Enpass/database"
+	"github.com/NoobforAl/Enpass/http/v1/controller"
+	"github.com/NoobforAl/Enpass/http/v1/router"
+	"github.com/NoobforAl/Enpass/lib/caching"
+	"github.com/NoobforAl/Enpass/loggers"
+	"github.com/NoobforAl/Enpass/validation"
+	"github.com/gin-gonic/gin"
+)
+
+func HttpApp() *gin.Engine {
+	logger := loggers.New()
+	validator := validation.New(logger)
+	cache := caching.New(env.GetLifeTime())
+	store, err := database.New(env.GetDSN(), logger)
+	if err != nil {
+		logger.Panic(err)
+	}
+
+	r := gin.Default()
+	baseConfig := &controller.BaseConfig{
+		GinApp:     r,
+		Stor:       store,
+		Logger:     logger,
+		Cache:      cache,
+		Validation: validator,
+	}
+
+	router.Default(baseConfig)
+	router.MainApi(baseConfig)
+	return r
+}
